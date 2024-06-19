7baa9ee03a2a61ddb780a8f4ff2d07ef16f1c62b
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-19 19:07:54 -0400

    React course from codevolution

---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hello = () => {
+    // return (
+    //  <div>
+    //     <h1>Hello Vishwas</h1>
+    //  </div>
+    // )
+    return React.createElement(
+        'div', 
+        null, 
+        React.createElement('h1', null, "Hello H1")
+    );
+}
+
+export default Hello;
