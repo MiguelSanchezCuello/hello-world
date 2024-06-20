5a59d9b085c206f0214c0beead624bfaaa0c673a
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-19 20:11:17 -0400

    Curso React codevolution

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 const Hello = () => {
+    // This is the JSX Version
     // return (
     //  <div>
     //     <h1>Hello Vishwas</h1>
     //  </div>
     // )
+
+    // This is the Component without using JSX
+    // The createElement method receives 3 parameters
+    // 1- The element to create
+    // 2- The props, we can use an object like {id:'hello', className:'dummyClass}
+    // 3- The string inside the element
     return React.createElement(
         'div', 
         null, 
         React.createElement('h1', null, "Hello H1")
     );
+
+    // These examples shows ho JSX makes it easier and more elegant(simpler).
 }
 
 export default Hello;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Component } from "react";
 
+// This is a Class component unlike Greet which is a functiona Component.
+
+
 class Welcome extends Component {
     render(){
         return <h1>Welcome {this.props.name} a.k.a. {this.props.heroName}</h1>;
