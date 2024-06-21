c7d923b0279eceb66ca5dfbc150bcded4ff97852
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-21 15:07:17 -0400

    Re editing the Counter component.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-       count: 0
-    }
-  }
-  
-  increment(){
-    this.setState({count: this.state.count + 2})
-  } 
-    render() {
+  render() {
     return (
-      <>
-        <div>
-            count - {this.state.count}
-        </div>
-        <button onClick={() => this.increment()}>
-            Increment
-        </button>
-      </>
+      <div>
+        count
+      </div>
     )
   }
 }
