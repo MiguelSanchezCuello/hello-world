6a920734186e3a98f0b97b42ae423210d436b1db
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-21 16:14:21 -0400

    Video 13 handling events

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick />
+      <ClassClick />
       {/* <Greet name="Bruce" heroName="Batman">
         <p>This is a child element</p>
       </Greet>
       <Greet name="Clark" heroName="Superman">
         <button>Action</button>
-      </Greet>
+      </Greet>*
       <Greet name="Diana" heroName="Wonder Woman"/>
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark" heroName="Superman" />
       <Welcome name="Diana" heroName="Wonder Woman"/>
       <Hello /> */}
       {/* <Message /> */}
-      <Counter />
+      {/* <Counter /> */}
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,41 @@
+// This is explaiend in video number 11
 import React, { Component } from 'react'
 
 class Counter extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count: 0
+    }
+  }
+
+  increment(){
+    // this.setState({
+    //   count: this.state.count + 1
+    //   }, () => {console.log(`Callback value ${this.state.count}`)
+    // })
+
+    this.setState( (prevState, props) => ({ 
+        count: prevState.count + 1
+      }))
+    console.log(this.state.count)
+  }
+  
+  incrementFive(){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+  }
+
   render() {
     return (
       <div>
-        count
+      <div>count - {this.state.count}      </div>
+      <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     )
   }
