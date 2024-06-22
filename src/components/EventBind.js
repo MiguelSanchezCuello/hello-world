675672cae80e4811878891393c2682150344e8aa
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-22 09:43:31 -0400

    Binding Event Handlers

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 
+// In this video No. 14 he explains 4 approaches on how to bind event handlers in React
+// 1- Binding in the render method (not sugggested due to performance implication)
+// 2- Binding with arrow function in the render method (easiest way to pass parameters)
+// 3- Binding in the class's constructor (suggested by react documentation by the time of the video 2017)
+// 4- Binding with a class property as an arrow function. (this was new back,2017, then but could be the one being used now)
 class EventBind extends Component {
     constructor(props) {
       super(props)
@@ -7,20 +12,39 @@ class EventBind extends Component {
       this.state = {
          message: 'Hello'
       }
+
+    //   this.clickHandler = this.clickHandler.bind(this)
     }
 
-    clickHandler(){
+    // clickHandler(){
+    //     this.setState({
+    //         message: 'Goodbye!'
+    //     })
+    //     console.log(this)
+    // }
+
+    // fourth approach
+    clickHandler = () => {
         this.setState({
-            message: 'Goodbye!'
+            message : 'Goodbye!'
         })
-        console.log(this)
     }
     
   render() {
     return (
       <div>
         <div>{this.state.message}</div>
-        <button onClick={this.clickHandler.bind(this)}>Click</button>
+        {/* // First approach for binding */}
+        {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */} 
+        {/* // Second approach using arrow functions */}
+        {/* <button onClick={() => this.clickHandler()}>Click</button> */}
+        
+        {/* // The third approach is to bind in the class construction. */}
+        {/* <button onClick={this.clickHandler}>Click</button> */}
+
+        {/* // The fourth approach defines a clickHandler with an arrow function. */}
+        <button onClick={this.clickHandler}>Click</button>
+
       </div>
     )
   }
