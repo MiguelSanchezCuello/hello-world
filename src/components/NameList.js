6bdb7a8a4347d530a1374bba5271fd3e89ff1b66
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-23 17:22:33 -0400

    The logic of the JSX was simplified by adding a nameList variable which contains the result of maping the elements to a list

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 function NameList() {
     const names = ['Bruce', 'Clark', 'Diana']
+    const nameList = names.map(name => <h2>{name}</h2>)
   return (
-    <div>
-        <h2>{names[0]}</h2>
-        <h2>{names[1]}</h2>
-        <h2>{names[2]}</h2>
-    </div>
+    <div> { nameList } </div>
   )
 }
 
