24f17fd6d8c99e28ad15081b8291a0851a73cf5e
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-23 16:28:07 -0400

    Conditional Rendering in React using if/else

---
+++ src/UserGreeting.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+   constructor(props) {
+     super(props)
+   
+     this.state = {
+        isLoggedIn: true
+     }
+   }
+   
+  
+    render() {
+        if (this.state.isLoggedIn) {
+            return <div>Welcome Coto</div>
+        } else {
+            return <div>Welcome Guest</div>
+        }
+
+
+
+
+
+        // return (
+        //     <div>
+        //         <div>Welcome React</div>
+        //         <div>Welcome Guest</div>
+        //     </div>
+        // )
+    }
+}
+
+export default UserGreeting
