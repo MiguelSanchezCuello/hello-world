4849c0bfdd4b6d26acd6463c775f2d5b8a6853f1
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-24 17:04:21 -0400

    Using CSS Modules to apply styles in React, we created the files appStyles.css and appStyles.module.css and look how we accessed to the classes in two different ways.

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ import UserGreeting from './UserGreeting';
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <Inline />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <Inline /> */}
       {/* <Stylesheet primary={true} /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
