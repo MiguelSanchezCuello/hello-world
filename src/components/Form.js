f2d2fa8f2821767e68ec26c0813359473876c426
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-24 19:26:49 -0400

    Added a controlled component textarea and the steps to follow when creating a controlled component.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 
+// There are three simple steps to use a Controlled Component, in this case for a Form.
+// 1 - Add the element HTML
+// 2 - Assign the component state to the element value
+// 3 - Assign an onChangeHandler taht updates the state.
+
 class Form extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-         username: ''
+         username: '',
+         comments: ''
       }
     }
 
-    HandleUsernameChange = (event) => {
+    handleUsernameChange = (event) => {
         this.setState({
             username: event.target.value
+        })
+    }
+
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
         })
     }
     
@@ -20,7 +32,16 @@ class Form extends Component {
       <form>
         <div>
             <label>Username:</label>
-            <input type="text" name="username" value={this.state.username} onChange={this.HandleUsernameChange}/>
+            <input type="text" name="username" 
+                value={this.state.username} 
+                onChange={this.handleUsernameChange}/>
+        </div>
+        <div>
+            <label>Comments</label>
+            <textarea value={this.state.comments} 
+                onChange={this.handleCommentsChange}>
+
+            </textarea>
         </div>
       </form>
     )
