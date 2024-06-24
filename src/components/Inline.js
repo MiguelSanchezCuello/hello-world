3912351bbbf616fedab61cabec7b7886b08e83ea
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-24 16:56:28 -0400

    Inline Styles with an object

---
+++ src/components/Inline.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// With the Inline styling we create an object that will have the styles and we apply it to the element inLine.
+
+const heading = {
+    fontSize: '72px',
+    color: 'blue'
+}
+
+function Inline() {
+  return (
+    <div>
+      <h1 style={heading}>Inline</h1>
+    </div>
+  )
+}
+
+export default Inline
