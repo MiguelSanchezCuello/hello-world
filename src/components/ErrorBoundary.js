b13a179711b5ab861388b8465f4b4d6dcf21f8f6
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-28 18:19:44 -0400

    The componentDidCatch method is used to log the information of the produced error, in this case we loged them into the console and we will see it repetitive because during development react logs error automatically.

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -16,6 +16,10 @@ class ErrorBoundary extends Component {
         return { hasError: true };
     }
 
+    componentDidCatch(error, info){
+        console.log(error, info);
+    }
+
   render() {
     if (this.state.hasError) {
         return <h1>Something went wrong</h1>;
