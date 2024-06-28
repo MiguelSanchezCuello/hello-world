56a81c9280fc8db4c220944e1925ee4eb30b0394
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-27 22:39:27 -0400

    memo component is to functional component what pure component are for class components

---
+++ src/components/MemoComp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MemoComp({name}) {
+  console.log("Rendering Memo Component")
+    return (
+    <div>
+      {name}
+    </div>
+  )
+console.log(object)
+}
+
+{name}{name}export default MemoComp
