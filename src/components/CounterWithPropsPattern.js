096f0b58eaa3c6f3a20484db8964ec2ad23da8c3
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-30 13:24:34 -0400

    Read the comments in the CounterWithPropsPattern.js file: // Keep in mind that there is a nother way to use the Render Props Pattern // by just passing the function as a prop and using the children, for instance: // <CounterWithPropsPattern> //   {(count, incrementCount) => ( //     <ClickCounterTwo count={count} incrementCount={incrementCount} /> //   )} // </CounterWithPropsPattern> // // Instead of using the render property and passing the component as a property to render. // and instead of using this.props.render(this.state.count, this.incrementCount) in the //CounterWithPropsPattern component we use this.props.children(this.state.count, this.incrementCount) // WE ARE STILL USING A PROP WHICH VALUE IS A FUNCTION TO RENDER UI AND SHARE FUNCTIONALITY.

--- src/components/CounterWithPropsPattern.js
+++ src/components/CounterWithPropsPattern.js
@@ -1,3 +1,16 @@
+// Keep in mind that there is a nother way to use the Render Props Pattern
+// by just passing the function as a prop and using the children, for instance:
+// <CounterWithPropsPattern> 
+//   {(count, incrementCount) => (
+//     <ClickCounterTwo count={count} incrementCount={incrementCount} />
+//   )}
+// </CounterWithPropsPattern> 
+//
+// Instead of using the render property and passing the component as a property to render.
+// and instead of using this.props.render(this.state.count, this.incrementCount) in the 
+// CounterWithPropsPattern component we use this.props.children(this.state.count, this.incrementCount)
+// WE ARE STILL USING A PROP WHICH VALUE IS A FUNCTION TO RENDER UI AND SHARE FUNCTIONALITY.
+
 import React, { Component } from 'react'
 
 class CounterWithPropsPattern extends Component {
