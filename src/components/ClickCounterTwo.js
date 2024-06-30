af4fc8604281e0032b3f53fbcf70322473a1e9c3
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-30 12:57:06 -0400

    Now we used the render props pattern to share the counter functionality among the components. created a new file CounterWithPropsPatter.js which will hold the counter functionality and the state to be shared, then removed these from the Hover  and CounterTwo components.

--- src/components/ClickCounterTwo.js
+++ src/components/ClickCounterTwo.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react'
 
 class ClickCounter2 extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       count: 0
-    }
-  }
-
-  incrementCount = () => {
-    this.setState(prevState => {
-        return {count: prevState.count + 1}
-    })
-  }
-  
-  render() {
+   render() {
     const { count } = this.state
     return (
       <div>
