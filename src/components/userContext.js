edc358edfa9984a003ef313d67bb968c225bc1b1
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-06-30 17:46:14 -0400

    A default value can be added to the context when creating the context in he React.createContext('default value here') method.
    The default value will be used when there is no value passed down from the three of components with the context's (when the Provider, does not provide)

--- src/components/userContext.js
+++ src/components/userContext.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const UserContext = React.createContext()
+const UserContext = React.createContext() // We could add a default value as theparameter when creating the context.
+// The default value will be used when no value is used in the userprovider.
 
 const UserProvier = UserContext.Provider
 const UserConsumer = UserContext.Consumer
